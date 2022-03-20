e0a593a54db2a273f4ce44c4aab8b87f00c9c383
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2022-03-19 23:08:05 -0500

    Update and rename TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java to TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/odometryRobotHardware.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/odometryRobotHardware.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * Created by User on 9/26/2017.
+ */
+
+public class RobotHardware
+{
+    //drive motor objects
+    public DcMotor motorRF = null;
+    public DcMotor motorLF = null;
+    public DcMotor motorRB = null;
+    public DcMotor motorLB = null;
+
+    //odometry encoder objects
+    public DcMotor leftEncoder = null;
+    public DcMotor rightEncoder = null;
+    public DcMotor perpendicularEncoder = null;
+
+    HardwareMap hwMap = null;
+    
+    public RobotHardware(HardwareMap ahwMap)
+    {
+        //dive motor init
+        motorRF = ahwMap.dcMotor.get("motorRF");
+        motorLF = ahwMap.dcMotor.get("motorLF");
+        motorRB = ahwMap.dcMotor.get("motorRB");
+        motorLB = ahwMap.dcMotor.get("motorLB");
+        
+        motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorLF.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorLB.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorRF.setPower(0);
+        motorLF.setPower(0);
+        motorRB.setPower(0);
+        motorLB.setPower(0);
+        
+        //odometry init (use the motors objects that the odometers are pluged into)
+        leftEncoder = motorLF;
+        rightEncoder = motorRF;
+        perpendicularEncoder = motorRB;
+    }
+    
+    public void resetDriveEncoders() 
+    {
+        motorLF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+}
