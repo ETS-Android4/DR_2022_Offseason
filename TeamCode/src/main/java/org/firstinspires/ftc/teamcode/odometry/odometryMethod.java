8abd601896da707005d49b970a1a3740a69619af
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2022-03-21 17:09:36 -0500

    Update odometryMethod.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/odometryMethod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/odometryMethod.java
@@ -12,9 +12,9 @@ public class odometryMethod extends LinearOpMode {
     private double inPerTick = 2.0 * Math.PI * R / N;
 
     //changes starting location (in inches)
-    public double X = 0;
-    public double Y = 0;
-    public double Heading = 0;
+    public double GlobalX = 0;
+    public double GlobalY = 0;
+    public double GlobalHeading = 0;
 
     //track encoder values between loops
     private int currentRightPos = 0;
@@ -28,7 +28,6 @@ public class odometryMethod extends LinearOpMode {
     {
         //init hardware map
         odometryRobotHardware robot = new odometryRobotHardware(hardwareMap);
-        MathFunctions mathFunctions = new MathFunctions();
 
         //record last loop's encoder reading
         oldRightPos = currentRightPos;
@@ -52,13 +51,83 @@ public class odometryMethod extends LinearOpMode {
 
         //add the robots movement this loop to the global location
         double theta = (dtheta / 2.0);
-        X += dx * Math.cos(theta) - dy * Math.sin(theta);
-        Y += dx * Math.sin(theta) + dy * Math.cos(theta);
-        Heading += dtheta;
+        GlobalX += dx * Math.cos(theta) - dy * Math.sin(theta);
+        GlobalY += dx * Math.sin(theta) + dy * Math.cos(theta);
+        GlobalHeading += dtheta;
 
         //makes heading 180 to -180
-        mathFunctions.angleWrapRad(Heading);
+        angleWrapRad(GlobalHeading);
     }
+    
+    
+    
+    
+    public void angleWrapRad(double angle)
+    {
+        while (angle > Math.PI)
+        {
+            angle -= Math.PI * 2;
+        }
+        while (angle < -Math.PI)
+        {
+            angle += Math.PI * 2;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    public void wait(double waitTime)
+    {
+        ElapsedTime time = new ElapsedTime();
+        
+        while (time <= waitTime)
+        {
+            refresh();
+        }
+    }
+    
+    
+    
+    
+    
+    public static void goToPos(double x, double y, double finalAngle, double moveSpeed, double turnSpeed, double followAngle)
+    {
+        odometryRobotHardware robot = new odometryRobotHardware(hardwareMap);
+        
+        reset();
+        
+        double distanceToTarget = Math.hypot(x - GlobalX, y - GlobalY);
+        
+        double absoluteAngleToTarget = Math.atan2(y - GlobalY, x - GlobalX);
+        
+        double reletiveAngleToTarget = angleWrapRad(absoluteAngleToTarget - GlobalHeading);
+        double reletiveXToTarget = Math.cos(reletiveAngleToTarget) * distanceToTarget;
+        double reletiveYToTarget = Math.sin(reletiveAngleToTarget) * distanceToTarget;
+        
+        double movementXpower = (reletiveXToTarget / (Math.abs(reletiveXToTarget) + Math.abs(reletiveYToTarget))) * moveSpeed;
+        double movementYpower = (reletiveYToTarget / (Math.abs(reletiveXToTarget) + Math.abs(reletiveYToTarget))) * moveSpeed;
+        
+        if (distanceToTarget > 10) 
+        {
+            double reletiveTurnAngle = reletiveAngleToTarget + followAngle;
+            double movementTurnPower = Range.clip(reletiveTurnAngle / Math.toRadians(10), -1, 1) * turnSpeed;
+        }
+        
+        else 
+        {
+            double movementTurnPower = ((finalAngle - GlobalHeading) /finalAngle)+ .2;
+        }
+        
+        robot.motorRF.setPower((-movementXpower - movementYpower) - (movementTurnPower));
+        robot.motorRB.setPower(-(-movementYpower + movementXpower) - (movementTurnPower));
+        robot.motorLB.setPower((movementXpower + movementYpower) - (movementTurnPower));
+        robot.motorLF.setPower((-movementYpower + movementXpower) - (movementTurnPower));
+    }
+    
+    
 
     @Override
     public void runOpMode() {}
